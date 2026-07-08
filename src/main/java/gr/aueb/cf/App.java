bf8f18a5daad4266f0320c8fdb72fdebbed4776c
a8anassis <a8anassis@gmail.com>
2026-07-08 20:01:36 +0300

    feat(app): implement JPQL and Criteria API to select active teachers

--- src/main/java/gr/aueb/cf/App.java
+++ src/main/java/gr/aueb/cf/App.java
@@ -1,10 +1,48 @@
 package gr.aueb.cf;
 
-/**
- * Hello world!
- */
+import gr.aueb.cf.model.Teacher;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.EntityTransaction;
+import jakarta.persistence.Persistence;
+import jakarta.persistence.criteria.CriteriaBuilder;
+import jakarta.persistence.criteria.CriteriaQuery;
+import jakarta.persistence.criteria.Root;
+
+
 public class App {
+
+    private static final EntityManagerFactory emf = Persistence.createEntityManagerFactory("schoolPU");
+
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        EntityManager em = emf.createEntityManager();
+        EntityTransaction tx = em.getTransaction();
+
+        try {
+
+            tx.begin();
+
+            // JPQL - Select active teachers
+            String sql = "SELECT t FROM Teacher t WHERE t.active = true";
+
+            // Criteria API - Select active teachers
+            CriteriaBuilder cb = em.getCriteriaBuilder();
+            CriteriaQuery<Teacher> cq = cb.createQuery(Teacher.class);
+            Root<Teacher> t = cq.from(Teacher.class);
+
+            // cq.select(t).where(cb.equal(t.get("active"), true));
+            cq.select(t).where(cb.isTrue(t.get("active")));
+
+
+            tx.commit();
+
+        } catch (RuntimeException e) {
+            if (tx.isActive()) tx.rollback();
+            throw e;
+        } finally {
+            em.close();
+            emf.close();
+        }
+
     }
 }
